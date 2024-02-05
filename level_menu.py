0322115a7196b502c0f8316d3f4ddcbc5492e8be
Dani4ka <daniilroitenberg@yandex.ru>
2024-02-05 18:40:07 +0300

    Небольшая доработка

--- level_menu.py
+++ level_menu.py
@@ -12,11 +12,13 @@ def level_menu():
 
     running = True
 
+    # загрузка шрифтов
     first_font = pygame.font.Font('data/font/minecraft-ten-font-cyrillic.ttf', 70)
     second_font = pygame.font.Font('data/font/minecraft-ten-font-cyrillic.ttf', 100)
     third_font = pygame.font.Font('data/font/minecraft-ten-font-cyrillic.ttf', 40)
     exit_font = pygame.font.Font('data/font/minecraft-ten-font-cyrillic.ttf', 50)
 
+    # загрузка текста
     text = first_font.render('LEVELS', True, GREY)
     text_zero = second_font.render('0', True, BACKGROUND_GREY)
     text_one = second_font.render('1', True, BACKGROUND_GREY)
@@ -29,9 +31,12 @@ def level_menu():
         for event in pygame.event.get():
             if pygame.QUIT == event.type:
                 running = False
+            # отслеживаем нажатие мыши
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x = event.pos[0]
                 y = event.pos[1]
+
+                # если нажата та или иная область запускаем уровень или выходим
                 if 45 < x < 245 and 170 < y < 370:
                     zero_level()
 
@@ -44,18 +49,22 @@ def level_menu():
                 if 350 < x < 550 and 470 < y < 670:
                     running = False
 
+        # отрисовываем кнопку уровня 0
         pygame.draw.rect(screen, GREY, (45, 170, 200, 200))
         screen.blit(level_text, (75, 180))
         screen.blit(text_zero, (110, 210))
 
+        # отрисовываем кнопку уровня 1
         pygame.draw.rect(screen, GREY, (350, 170, 200, 200))
         screen.blit(level_text, (380, 180))
         screen.blit(text_one, (415, 210))
 
+        # отрисовываем кнопку уровня 2
         pygame.draw.rect(screen, GREY, (45, 470, 200, 200))
         screen.blit(level_text, (75, 480))
         screen.blit(text_second, (110, 510))
 
+        # отрисовываем кнопку выхода
         pygame.draw.rect(screen, GREY, (350, 470, 200, 200))
         screen.blit(exit_text, (383, 520))
 
